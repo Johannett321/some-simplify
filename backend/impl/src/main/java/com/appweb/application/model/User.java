9d247ca416dc1d4e8a669d861ecd1bcc3d7f8ff2
johannett321 <johan.svartdal30@gmail.com>
2025-12-11 18:01:44 +0100

    Removed currentTenant from User model, as it is included in header instead

--- backend/impl/src/main/java/com/appweb/application/model/User.java
+++ backend/impl/src/main/java/com/appweb/application/model/User.java
@@ -26,9 +26,6 @@ public class User {
     private String firstName;
     private String lastName;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    private Tenant currentTenant;
-
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<ApplicationRole> applicationRoles;
 }
