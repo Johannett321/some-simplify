27d014a16da7ba43a69e510ed82b288c85616390
johannett321 <johan.svartdal30@gmail.com>
2025-12-11 18:19:19 +0100

    Improving tenant with picker and ideas from bilago

---
+++ frontend/src/components/auth/TenantPickerPage.tsx
@@ -0,0 +1,73 @@
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {Item, ItemActions, ItemContent, ItemDescription, ItemTitle} from "@/components/ui/item.tsx";
+import {ChevronRightIcon} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
+import {Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyTitle} from "@/components/ui/empty.tsx";
+import {useNavigate} from "react-router-dom";
+import {type TenantTO} from "@/api";
+
+interface TenantPickerPageProps {
+    tenants: TenantTO[]
+    onSelectTenant: (organization: TenantTO) => void
+}
+
+export const TenantPickerPage = ({tenants, onSelectTenant}: TenantPickerPageProps) => {
+    const navigate = useNavigate()
+    return (
+        <div className={"w-screen h-screen flex justify-center items-center"}>
+            <img className={"absolute top-0 left-0 w-screen h-screen object-cover -z-10"} src={"/registerphoto.jpg"}/>
+            <Card className={"w-[400px] h-[400px]"}>
+                {tenants.length > 0 ? (
+                    <>
+                        <CardHeader>
+                            <CardTitle>Velg ditt foretak</CardTitle>
+                            <CardDescription>Velg foretaket du vil logge inn på</CardDescription>
+                        </CardHeader>
+                        <CardContent className={"overflow-y-auto space-y-2"}>
+                            {tenants.map(o => (
+                                <Item
+                                    key={o.id}
+                                    className={"hover:bg-stone-50 transition-all cursor-pointer"}
+                                    onClick={() => onSelectTenant(o)}
+                                    variant={"outline"}
+                                >
+                                    <ItemContent>
+                                        <ItemTitle>{o.name}</ItemTitle>
+                                        <ItemDescription>{o.orgNumber} {o.city}</ItemDescription>
+                                    </ItemContent>
+                                    <ItemActions>
+                                        <ChevronRightIcon className="size-4" />
+                                    </ItemActions>
+                                </Item>
+                            ))}
+                        </CardContent>
+                        <CardFooter className={"flex justify-between"}>
+                            <Button variant={"destructive"}>
+                                Logg ut
+                            </Button>
+                            <Button onClick={() => navigate("/opprett-foretak")} variant={"default"}>
+                                Opprett nytt foretak
+                            </Button>
+                        </CardFooter>
+                    </>
+                ) : (
+                    <Empty>
+                        <EmptyHeader>
+                            <EmptyTitle>Opprett ditt første foretak</EmptyTitle>
+                            <EmptyDescription>
+                                Du har ingen foretak. Kom i gang ved å lage ditt første foretak
+                            </EmptyDescription>
+                        </EmptyHeader>
+                        <EmptyContent>
+                            <Button onClick={() => navigate("/opprett-foretak")}>
+                                Opprett foretak
+                            </Button>
+                        </EmptyContent>
+                    </Empty>
+                )}
+            </Card>
+        </div>
+    )
+}
+
+export default TenantPickerPage
--- frontend/src/providers/TenantProvider.tsx
+++ frontend/src/providers/TenantProvider.tsx
@@ -1,6 +1,9 @@
 import {createContext, type ReactNode, useEffect, useState} from "react";
 import {TenantApi, type TenantTO} from "@/api";
 import apiConfig from "@/config/ApiConfig.ts";
+import TenantPickerPage from "@/components/auth/TenantPickerPage.tsx";
+import axios from "axios";
+
 
 type TenantProviderContextType = {
     tenant: TenantTO | null;
@@ -15,23 +18,55 @@ export const TenantContext = createContext<TenantProviderContextType>({
 
 export const TenantProvider = ({children}: { children: ReactNode }) => {
     const [tenantApi] = useState<TenantApi>(new TenantApi(apiConfig))
+
+    const [allTenants, setAllTenants] = useState<TenantTO[] | null>(null)
     const [tenant, setTenant] = useState<TenantTO | null>(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        loadTenantData()
+        reloadTenants()
     }, []);
 
-    const loadTenantData = () => {
-        tenantApi.getTenant().then(res => {
-            setTenant(res.data)
-        }).finally(() => setLoading(false))
+    const reloadTenants = async () => {
+        const tenantsResponse = await tenantApi.getTenants()
+        const tenants = tenantsResponse.data
+        setAllTenants(tenants)
+        if (!tenants) {
+            setLoading(false);
+            return
+        }
+
+        const tenantId = localStorage.getItem("tenant");
+        const tenant = tenants?.find(o => o.id == tenantId)
+        if (!tenant) {
+            localStorage.removeItem("tenant");
+            setLoading(false);
+            return
+        }
+
+        setTenant(tenant)
+
+        setLoading(false)
     }
 
-    if (loading) {
+    const onSelectTenant = async (tenant: TenantTO) => {
+        localStorage.setItem("tenant", tenant.id!)
+        axios.defaults.headers.common['X-Tenant-ID'] = localStorage.getItem("tenant");
+        setTenant(tenant)
+    }
+
+    if (loading || !allTenants) {
         return (
             <>
                 Loading...
+            </>
+        )
+    }
+
+    if (!tenant) {
+        return (
+            <>
+                <TenantPickerPage tenants={allTenants} onSelectTenant={onSelectTenant} />
             </>
         )
     }
