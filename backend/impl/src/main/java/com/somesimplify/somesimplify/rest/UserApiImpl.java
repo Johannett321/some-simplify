486c69d4014a539fc78c4cb8d8b6aa21ca4ba0af
johannett321 <johan.svartdal30@gmail.com>
2025-12-12 12:12:05 +0100

    Laget getUser endepunkt

--- backend/impl/src/main/java/com/somesimplify/somesimplify/rest/UserApiImpl.java
+++ backend/impl/src/main/java/com/somesimplify/somesimplify/rest/UserApiImpl.java
@@ -1,4 +1,26 @@
 package com.somesimplify.somesimplify.rest;
 
-public class UserApiImpl {
+import com.somesimplify.api.UserApi;
+import com.somesimplify.model.UserTO;
+import com.somesimplify.somesimplify.mapper.UserMapper;
+import com.somesimplify.somesimplify.model.User;
+import com.somesimplify.somesimplify.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class UserApiImpl implements UserApi {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Override
+    public ResponseEntity<UserTO> getUser() {
+        User currentUser = userService.getCurrentUser();
+        return ResponseEntity.ok(userMapper.toUserResponseTO(currentUser));
+    }
 }
