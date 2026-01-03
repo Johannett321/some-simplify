67540a8d7f56b009aabf14149f230104d85a4535
johannett321 <johan.svartdal30@gmail.com>
2026-01-03 20:05:40 +0100

    Turned off auto create images

--- backend/impl/src/main/java/com/somesimplify/somesimplify/service/ImageGenerationService.java
+++ backend/impl/src/main/java/com/somesimplify/somesimplify/service/ImageGenerationService.java
@@ -26,7 +26,8 @@ public class ImageGenerationService {
     private String apiKey;
 
     private final ChatModel chatModel;
-    @Scheduled(fixedRate = 1000 * 60 * 60* 24)
+    @Scheduled(cron = "0 0 0 * * *")
+    //@Scheduled(fixedRate = 1000 * 60 * 60* 24)
     private void generatePrompt() {
         log.info("Generating prompt for image generation...");
         String system = """
